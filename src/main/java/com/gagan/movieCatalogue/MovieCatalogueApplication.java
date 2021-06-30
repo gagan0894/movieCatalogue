a9635ae3f52408dde9df0f74caa90b924ada10b5
gagan0894 <gagandeep0894@gmail.com>
2021-06-30 23:30:11 +0530

    code for movieCatalogue application

--- src/main/java/com/gagan/movieCatalogue/MovieCatalogueApplication.java
+++ src/main/java/com/gagan/movieCatalogue/MovieCatalogueApplication.java
@@ -1,14 +1,58 @@
 package com.gagan.movieCatalogue;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MovieCatalogueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueApplication.class, args);
+	}
+
+	@Bean(initMethod = "migrate")
+	Flyway flyway() {
+		Flyway flyway = new Flyway();
+		flyway.setBaselineOnMigrate(true);
+		flyway.setDataSource("jdbc:postgresql://localhost:5432/movies", "postgres", "gagan0894");
+		flyway.setSchemas("movies_catalogue");
+		return flyway;
+	}
+	@Bean
+	public Docket swaggerConfiguration()
+	{
+		//Return a prepared docket instance
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/movieCatalogue/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.gagan"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+
+	private ApiInfo apiDetails()
+	{
+		return new ApiInfo(
+				"Movie Catalogue API",
+				"API to demonstrate basic operations on movie catalogue application",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Gagandeep Singh", "https://github.com/gagan0894/movieCatalogue", "gagandeep0894@gmail.com"),
+				"API License",
+				"https://www.mongodb.com",
+				Collections.emptyList());
 	}
 
 }
--- src/main/java/com/gagan/movieCatalogue/controller/MovieCatalogueController.java
+++ src/main/java/com/gagan/movieCatalogue/controller/MovieCatalogueController.java
@@ -3,6 +3,8 @@ package com.gagan.movieCatalogue.controller;
 import com.gagan.movieCatalogue.model.AddMovieRequest;
 import com.gagan.movieCatalogue.model.Genre;
 import com.gagan.movieCatalogue.service.MovieCatalogueService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -13,39 +15,56 @@ import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
+@RequestMapping("/movieCatalogue")
 public class MovieCatalogueController {
 
     @Autowired
     private MovieCatalogueService movieCatalogueService;
 
     @PostMapping("/addMovie")
-    public HttpEntity addMovie(@RequestBody AddMovieRequest addMovieRequest)
+    @ApiOperation(value="Add a movie to the catalogue",
+            notes="Contains the information for both movie and it genre, to maintain the relation ",
+            response=ResponseEntity.class)
+    public ResponseEntity addMovie(@ApiParam(value="contains the information for a genre and the list of movie to be added to that genre",required=true)
+            @RequestBody AddMovieRequest addMovieRequest)
     {
         movieCatalogueService.addMovie(addMovieRequest.getGenre());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/retrieveAllMovies")
+    @ApiOperation(value="Find all the movies in the catalogue",
+            notes="finds all the movie with it genres, returns list of Genre")
     public List<Genre> getAllMovies()
     {
         return movieCatalogueService.getAllMovies();
     }
 
     @GetMapping("/retrieveByGenre/{genreName}")
+    @ApiOperation(value="Find all the movies in the catalogue by a genre",
+            notes="finds all the movie for a genre, returns list of Genre")
     public List<Genre> getMoviesByGenreName(@PathVariable String genreName)
     {
         return movieCatalogueService.getMoviesByGenreName(genreName);
     }
 
     @DeleteMapping("/removeGenre/{genreName}")
-    public HttpEntity removeGenre(@PathVariable String genreName)
+    @ApiOperation(value="Deletes the genre and the assoicated movies",
+            notes="Deletes the genre and the assoicated movies, maintaining the relationship between movies and genre",
+            response=ResponseEntity.class)
+    public ResponseEntity removeGenre(@ApiParam(value="genre category to be removed",required=true)
+            @PathVariable String genreName)
     {
          movieCatalogueService.removeGenre(genreName);
          return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/removeMovie/{movieName}")
-    public HttpEntity removeMovie(@PathVariable String movieName)
+    @ApiOperation(value="Deletes a particular movie",
+            notes="Deletes a specific movie from the catalogue",
+            response=ResponseEntity.class)
+    public ResponseEntity removeMovie(@ApiParam(value="movie to be removed",required=true)
+            @PathVariable String movieName)
     {
         movieCatalogueService.removeMovie(movieName);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
